c5b78106d11cab6ea0613b773d4401b42704bb93
Claudia Apaza <claudhia.am@gmail.com>
2021-03-27 10:05:16 -0300

    Cambios para el 2do desafio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,56 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>E-commerce</h1>
+      <h1>E-commerce Start-it</h1>
+      <nav className="navigation-menu">
+        <ul>
+        <li>
+            <a
+              href="https://startit.jgt.com.ar/index.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://startit.jgt.com.ar/servicios.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://startit.jgt.com.ar/trabajos.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Trabajos
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://startit.jgt.com.ar/nosotros.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Nosotros
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://startit.jgt.com.ar/contactanos.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Contáctanos
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
